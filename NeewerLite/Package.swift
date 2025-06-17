d59ac811945ede2c38c1122916a6296159c55ef4
Xu Lian <lianxu@me.com>
2025-06-16 20:36:20 -0700

    test

---
+++ NeewerLite/Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NeewerLite",
+    defaultLocalization: "en",
+    platforms: [
+        .macOS(.v11),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "NeewerLite",
+            targets: ["NeewerLite"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/httpswift/swifter.git", from: "1.5.0")
+        .package(url: "https://github.com/sparkle-project/Sparkle.git", from: "1.27.3")
+        .package(url: "https://github.com/apple/swift-atomics.git", from: "1.3.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "NeewerLite",
+            dependencies: ["Swifter", "Sparkle", "swift-atomics"],
+            path: "NeewerLite"),
+        .testTarget(
+            name: "NeewerLiteTests",
+            dependencies: ["NeewerLite"],
+            path: "NeewerLiteTests"),
+    ]
+)
